b7aa157fe9db5c5b554b75236a766b232075d133
Bogdan Manole <manoleb@gmail.com>
2026-05-29 12:39:14 +0300

    adding offchain

--- lib/Scripts.hs
+++ lib/Scripts.hs
@@ -9,11 +9,15 @@
 -- | Scripts used for testing
 module Scripts (
   pingPongValidatorScript,
+  playPingPongRound,
   PingPong.PingPongRedeemer (..),
   PingPong.PingPongState (..),
 ) where
 
+import Cardano.Api (NetworkId)
 import Cardano.Api qualified as C
+import Convex.BuildTx (MonadBuildTx)
+import Convex.BuildTx qualified as BuildTx
 import Convex.PlutusTx (compiledCodeToScript)
 import PlutusTx (BuiltinData, CompiledCode)
 import PlutusTx qualified
@@ -28,3 +32,33 @@ pingPongValidatorScript = compiledCodeToScript pingPongValidatorCompiled
 
 plutusScript :: (C.IsPlutusScriptLanguage lang) => C.PlutusScript lang -> C.Script lang
 plutusScript = C.PlutusScript C.plutusScriptVersion
+
+playPingPongRound
+  :: forall era m
+   . ( C.IsBabbageBasedEra era
+     , C.HasScriptLanguageInEra C.PlutusScriptV3 era
+     )
+  => (MonadBuildTx era m)
+  => NetworkId
+  -> C.Lovelace
+  -> PingPong.PingPongRedeemer
+  -> C.TxIn
+  -> m ()
+playPingPongRound networkId value redeemer txi = do
+  let witness _ =
+        C.ScriptWitness C.ScriptWitnessForSpending $
+          BuildTx.buildScriptWitness
+            pingPongValidatorScript
+            (C.ScriptDatumForTxIn $ Nothing)
+            redeemer
+  BuildTx.setScriptsValid >> BuildTx.addInputWithTxBody txi witness
+  BuildTx.payToScriptInlineDatum
+    networkId
+    (C.hashScript (plutusScript pingPongValidatorScript))
+    ( case redeemer of
+        PingPong.Ping -> PingPong.Pinged
+        PingPong.Pong -> PingPong.Ponged
+        PingPong.Stop -> PingPong.Stopped
+    )
+    C.NoStakeAddress
+    (C.lovelaceToValue value)
